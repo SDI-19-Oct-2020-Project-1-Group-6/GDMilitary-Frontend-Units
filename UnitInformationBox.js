4c55bbc23c7c395cb56384f7ecdf96b3bb0140b9
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-19 14:50:40 -0800

    update api usage and formatting

--- UnitInformationBox.js
+++ UnitInformationBox.js
@@ -7,19 +7,41 @@ export default class UnitInformationBox extends React.Component {
             info: {afscs:[] }
         }
     }
+    initializeInfo() {
+        this.setState({info:{afscs:[]}});
+    }
     async componentDidMount() {
-      fetch(`http://unitsdb:4444/unit/${this.props.unit}`)
+      fetch(`http://localhost:4444/units/${this.props.unit}`)
       .then(response => response.json())
-      .then(data => this.setState({ info: data }));
+      .then(data => {
+          if (data.code===200) {
+            this.setState({ info: data.result})
+          }
+      });
     }
     render() {
+        /*
+        <div class="card">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+*/
         return (
-            <div>
-                <img src={this.state.info.logo} alt={`Logo for ${this.state.info.name}`} width='100px' height='100px'/><br />
-                Name {this.state.info.name} <br />
-                Location {this.state.info.location} <br />
-                Size {this.state.info.size} <br />
-                AFSC {this.state.info.afscs.join()} <br />
+            
+            <div className="card">
+                <div className="card-body">
+                <h4 className="card-title">Unit Information</h4>
+                {/* <img src={this.state.info.logo} alt={`Logo for ${this.state.info.name}`} width='100px' height='100px'/><br /> */}
+                <p className="card-text">Name: {this.state.info.name} </p>
+                <p className="card-text">Location: {this.state.info.location} </p>
+                <p className="card-text">Population: {this.state.info.size} </p>
+                <p className="card-text">AFSCs Available: {this.state.info.afscs.join()} </p>
+                </div>
             </div>
 
         )
