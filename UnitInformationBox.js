0a7269b11722bf7563c2e3785f6daf9604788aac
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-23 09:46:41 -0800

    semantic formatting. remove name edit button

--- UnitInformationBox.js
+++ UnitInformationBox.js
@@ -29,7 +29,7 @@ export default class UnitInformationBox extends React.Component {
         let data = await fetch(`http://localhost:4444/units/${this.props.unit}`);
         data = await data.json();
         if (data.code===200) {
-            return {...data.result,afscs:data.result.afscs.join()};
+            return {...data.result,afscs:data.result.afscs.join(', ')};
         }else{
             throw new Error("Failed to read data from API");
         }
@@ -53,13 +53,22 @@ export default class UnitInformationBox extends React.Component {
         this.setState({modify:currentModify})
     }
     handleSubmit=(field) => {
+        let newData ={};
+        newData[field] = this.state.modify[field];
         ///TODO SUBMIT CHANGES TO DB THEN REQUEST NEW INFO
         fetch(`http://localhost:4444/units/${this.props.unit}`,{
             method: 'PATCH',
             headers:{
                 "Content-Type":"application/json"
+            },
+            body: JSON.stringify(newData)
+        }).then(data=>data.json()).then(data=>{
+            if (field==='name') {
+                this.props.onUpdateName();
+            }else{
+                this.getUnitInfo().then(data=>this.setState({info:data}));
             }
-        }).then(data=>data.json()).then(data=>console.log(data));
+        }).catch(data=>console.log(data));
         //clear entries and undo editing state. easiest to just cancel
         this.handleCancel(field);
     }
@@ -90,42 +99,32 @@ export default class UnitInformationBox extends React.Component {
             )
         }else{
             return (
-                <div className="input-group mb-3">
-                    <input type="text" readOnly className="form-control-plaintext"  value={this.state.info[field]} />
-                    <div className="input-group-append">
-                        <button type='button' onClick={()=>this.handleStartEditing(field)}>Edit</button>
-                    </div>
+                <div className="ui input">
+                    <label className="ui right labeled">{this.state.info[field]}</label>
+                    <button className="ui label mini button" type='button' onClick={()=>this.handleStartEditing(field)}>Edit</button>
                 </div>
             )
         }
     }
     render() {
-        /*
-        <div class="card">
-  <div class="card-body">
-    <h4 class="card-title">Card title</h4>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-*/
+
         return (
             
-            <div className="card">
-                <div className="card-body">
-                <h4 className="card-title">Unit Information</h4>
-                {/* <img src={this.state.info.logo} alt={`Logo for ${this.state.info.name}`} width='100px' height='100px'/><br /> */}
-                
-                
-                
-                
-                <div className="card-text">{this.labelOrEdit('name')} </div>
-                <div className="card-text">Location: {this.labelOrEdit('location')} </div>
-                <div className="card-text">Population: {this.labelOrEdit('size')} </div>
-                <div className="card-text">AFSCs Available: {this.labelOrEdit('afscs')} </div>
+            <div className="ui fluid card">
+                <div className="content">
+                <h4 className="header">Unit Information</h4>
                 </div>
+                <div className="content">
+                {/* <img src={this.state.info.logo} alt={`Logo for ${this.state.info.name}`} width='100px' height='100px'/><br /> */}
+                <div className="ui right labled input">
+                    <div className="ui input">
+                        <label className="ui right labeled">{this.state.info['name']}</label>
+                    </div> 
+                </div>
+                <div className="ui small feed">Location: {this.labelOrEdit('location')} </div>
+                <div className="ui small feed">Population: {this.labelOrEdit('size')} </div>
+                <div className="ui small feed">AFSCs Available: {this.labelOrEdit('afscs')} </div>
+                </div>  
             </div>
 
         )
